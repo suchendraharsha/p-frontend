a0c43619b2d438ced2a1f16564f5462b2a24009c
SudevaHarsha <sudevaharsha1@gmail.com>
2023-07-09 10:33:13 +0530

    second

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const userModel = require("./model/UserModel");
+const mongoose = require("mongoose");
 
-const mysql = require('mysql');
+const dotenv = require("dotenv");
+
+/* const mysql = require('mysql'); */
 
 const cors = require('cors');
 
-const { check, validationResult } = require('express-validator');
+/* const { check, validationResult } = require('express-validator'); */
 
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+/* const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
@@ -30,9 +35,30 @@ app.post('/signup', (req, res) => {
         }        
         return res.json(data);    
     })
-})
+}) */
 
-app.post('/login',[    
+app.post('/signup', async(req, res) => {
+    try{
+    const {name,email,password}=req.body  
+    const data = await userModel.create({name,email,password});
+      
+    return res.json(data);
+    } catch(error){
+        return res.json("Error")
+    }
+})
+app.post('/login',async(req, res) => {
+    try{
+    const {email,password}=req.body  
+    const data = await userModel.findOne({email,password});
+      
+    return res.json("Success");
+    } catch(error){
+        return res.json("Error")
+    }
+}
+)
+/* app.post('/login',[    
     check('email', "Emaill length error").isEmail().isLength({min: 10, max:30}),    
     check('password', "password length 8-10").isLength({min: 8, max: 20})], (req, res) => {    
         const sql = "SELECT * FROM login WHERE email = ? AND password = ?";    
@@ -53,8 +79,20 @@ app.post('/login',[
             }        
         }            
     })
-})
+}) */
 
-app.listen(8081, ()=> {    
+/* app.listen(8081, ()=> {    
     console.log("listening");
-})
+}) */
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 8081;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
